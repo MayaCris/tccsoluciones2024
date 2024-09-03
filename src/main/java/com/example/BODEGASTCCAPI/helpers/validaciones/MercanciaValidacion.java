5e5f7460c882f77d0e24c0ddf3dd85019dd27c22
Cristian Maya G <cmaya@cesde.net>
2024-09-03 08:42:47 -0500

    Add date validation

--- src/main/java/com/example/BODEGASTCCAPI/helpers/validaciones/MercanciaValidacion.java
+++ src/main/java/com/example/BODEGASTCCAPI/helpers/validaciones/MercanciaValidacion.java
@@ -1,7 +1,10 @@
 package com.example.BODEGASTCCAPI.helpers.validaciones;
 
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDate;
 
+@Component
 public class MercanciaValidacion {
 
     public boolean validarVolumen(Double mercanciaVolumen){
--- src/main/java/com/example/BODEGASTCCAPI/servicios/MercanciaServicio.java
+++ src/main/java/com/example/BODEGASTCCAPI/servicios/MercanciaServicio.java
@@ -1,10 +1,12 @@
 package com.example.BODEGASTCCAPI.servicios;
 
+import com.example.BODEGASTCCAPI.helpers.validaciones.MercanciaValidacion;
 import com.example.BODEGASTCCAPI.modelos.Mercancia;
 import com.example.BODEGASTCCAPI.repositorios.IMercanciaRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,11 +18,23 @@ public class MercanciaServicio {
     @Autowired
     IMercanciaRepositorio mercanciaRepositorio;
 
+    @Autowired
+    MercanciaValidacion mercanciaValidacion;
+
     //guardar
     public Mercancia almacenarMercancia(Mercancia datosMercancia) throws Exception {
         try {
             //aplicar validaciones a los datos recibidos
             //Si sale bien la validacion llamo al repositorio para guardar los datos
+            if (!this.mercanciaValidacion.validarPeso(datosMercancia.getPeso())){
+                throw new Exception("El peso no puede ser menor o igual a 0");
+            }
+            if (!this.mercanciaValidacion.validarVolumen(datosMercancia.getVolumen())){
+                throw new Exception("El volumen no puede ser menor o igual a 0");
+            }
+            if (!this.mercanciaValidacion.validarFechaIngreso(datosMercancia.getFechaIngreso(), LocalDate.now())){
+                throw new Exception("La fecha de ingreso no puede ser mayor a la fecha actual");
+            }
             return mercanciaRepositorio.save(datosMercancia);
             }
         catch (Exception e){
